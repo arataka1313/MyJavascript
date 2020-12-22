cb131540b258de649235837d38fb4326de52242c
Taka <42265874+arataka1313@users.noreply.github.com>
2020-12-22 23:04:18 +0900

    js practice
    enjoy js.

---
+++ MYJavaScript/基本.js
@@ -0,0 +1,29 @@
+//console表示
+console.log("good morning");
+
+console.log("good afternoon");
+
+console.log("good evening");
+
+//変数
+//number型
+var num01=123;
+var num02=1.23;
+console.log(num01+typeof(num01)); //typeofで型を標示．int型やfloat型はなく．number型だけ.
+console.log(num02+typeof(num02));
+//string型 文字列型
+var string_a="hello world"
+
+console.log(string_a);
+console.log(typeof(string_a));
+
+//bool型
+var a=10;
+var b=1;
+var num_bool;
+
+num_bool=(a<b); //a<bならtrue,a>bならfalse
+console.log(num_bool); 
+console.log(typeof(num_bool));
+
+//配列
